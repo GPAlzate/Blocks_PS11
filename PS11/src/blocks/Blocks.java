5734d9935f3fbc0bad24a3368dbf2c3a10dcab5c
kristinecho <kristinecho@192.168.1.9>
2020-03-10 19:17:38 -0700

    Working on maxHeightDP, dynamic programming solution

--- PS11/src/blocks/Blocks.java
+++ PS11/src/blocks/Blocks.java
@@ -6,208 +6,256 @@ import java.util.Collections;
 import java.util.Scanner;
 
 /**
- * A program which builds the tallest tower possible out of a collection
- * of rectangular blocks
+ * A program which builds the tallest tower possible out of a collection of
+ * rectangular blocks
  * 
- *  A block can be stacked on top of another block if and only if the
- *  two dimensions on the base of the top block are smaller than the two dimensions on
- *  the base of the lower block
+ * A block can be stacked on top of another block if and only if the two
+ * dimensions on the base of the top block are smaller than the two dimensions
+ * on the base of the lower block
  * 
  * @authors Kristine Cho and Mercy Bickell
  * @version 3/12/20
  *
  */
 public class Blocks {
-	
+
 	ArrayList<Block> setOfBlocks = new ArrayList<>();
-	
+
 	/**
-	 * reads in input and 
-	 * runs the algorithm to find the tallest possible tower using the given blocks
+	 * reads in input and runs the algorithm to find the tallest possible tower
+	 * using the given blocks
 	 * 
-	 * @param args command line arguments
-	 * @throws IOException if file not found
+	 * @param args
+	 *            command line arguments
+	 * @throws IOException
+	 *             if file not found
 	 */
 	public static void main(String[] args) throws IOException {
 		File infile = new File(args[0]);
 		File outfile = new File(args[1]);
 		Blocks runner = new Blocks();
-		
+
 		try {
-			Scanner readIn = new Scanner(infile); //to read in from infile
-			int arrayLength =  Integer.valueOf(readIn.nextLine());
+			Scanner readIn = new Scanner(infile); // to read in from infile
+			int arrayLength = Integer.valueOf(readIn.nextLine());
 			Block[] blockTypes = new Block[arrayLength];
-			
-			//read in the blocks from the file
-			for(int i = 0; i < arrayLength; i++) {
+
+			// read in the blocks from the file
+			for (int i = 0; i < arrayLength; i++) {
 				String currentBlock = readIn.nextLine();
 				String[] dimensions = currentBlock.split(" ");
-				
+
 				int l = Integer.parseInt(dimensions[0]);
 				int w = Integer.parseInt(dimensions[1]);
 				int h = Integer.parseInt(dimensions[2]);
 				blockTypes[i] = runner.new Block(w, l, h);
 			}
-			
+
 			runner.setOfBlocks = runner.getVariations(blockTypes);
-			
-			//now do stuff with all the possible orientations of the blocks
-			//sort the blocks in order from largest area to smallest
+
+			// now do stuff with all the possible orientations of the blocks
+			// sort the blocks in order from largest area to smallest
 			Collections.sort(runner.setOfBlocks);
-			
-			//for debugging
-			for(int i = 0; i < runner.setOfBlocks.size(); i++) {
+
+			// for debugging
+			for (int i = 0; i < runner.setOfBlocks.size(); i++) {
 				System.out.println(runner.setOfBlocks.get(i));
 			}
-			
-			
+
 			ArrayList<Integer> maxHeights = new ArrayList<Integer>();
-			for(int i = 0; i < runner.setOfBlocks.size(); i++) {
+			for (int i = 0; i < runner.setOfBlocks.size(); i++) {
 				maxHeights.add(runner.maxHeightRecur(runner.setOfBlocks.get(i)));
 			}
-			
-			/* for debugging
-			for(int i = 0; i < maxHeights.size(); i++) {
-				System.out.println(maxHeights.get(i));
-			}*/
-			
-			int maxHeight = runner.finalMaxHeight(maxHeights); //run alg here
-			System.out.println(maxHeight);
-			
+
 			/*
-			ArrayList<Block> tallestTowerBlocks = new ArrayList<>(); //store tallest blocks somehow in an array
-			int numBlocks = tallestTowerBlocks.size();
-			
-			readIn.close();
-			
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outfile)); //to write to outfile
-			
-			writer.write(numBlocks); //first line of outfile should be number of blocks in tallest tower
-			writer.newLine();
-			
-			// loop which writes out all the blocks in the tallest possible tower to the outfile
-			for(int i = 0; i < numBlocks; i++) {
-				writer.write(tallestTowerBlocks.get(i).toString());
-				writer.newLine();
-			}
-			
-			writer.close();
-			*/
-		}
-		catch (FileNotFoundException e){
+			 * for debugging for(int i = 0; i < maxHeights.size(); i++) {
+			 * System.out.println(maxHeights.get(i)); }
+			 */
+
+			int maxHeight = runner.finalMaxHeight(maxHeights); // run alg here
+			System.out.println(maxHeight);
+
+			/*
+			 * ArrayList<Block> tallestTowerBlocks = new ArrayList<>(); //store tallest
+			 * blocks somehow in an array int numBlocks = tallestTowerBlocks.size();
+			 * 
+			 * readIn.close();
+			 * 
+			 * BufferedWriter writer = new BufferedWriter(new FileWriter(outfile)); //to
+			 * write to outfile
+			 * 
+			 * writer.write(numBlocks); //first line of outfile should be number of blocks
+			 * in tallest tower writer.newLine();
+			 * 
+			 * // loop which writes out all the blocks in the tallest possible tower to the
+			 * outfile for(int i = 0; i < numBlocks; i++) {
+			 * writer.write(tallestTowerBlocks.get(i).toString()); writer.newLine(); }
+			 * 
+			 * writer.close();
+			 */
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * method which, given a list of blocks, returns a list with all possible block
 	 * configurations by looping through each given block, finding its 3 rotations,
-	 * and adding it to our list 
+	 * and adding it to our list
 	 * 
 	 * @param inputBlocks
 	 * @return an arrayList of all possible blocks to use in the tower
 	 */
-	public ArrayList<Block> getVariations(Block[] inputBlocks){
+	public ArrayList<Block> getVariations(Block[] inputBlocks) {
 		ArrayList<Block> allBlocks = new ArrayList<Block>();
-		for(int i = 0; i < inputBlocks.length; i++) {
+		for (int i = 0; i < inputBlocks.length; i++) {
 			allBlocks.add(inputBlocks[i].rotation1());
 			allBlocks.add(inputBlocks[i].rotation2());
 			allBlocks.add(inputBlocks[i].rotation3());
 		}
 		return allBlocks;
 	}
-	
+
+	// FIXME This is not correct. Delete for submission
 	public int maxHeightRecurK(ArrayList<Block> blocksInTower) {
-		
+
 		// Base case where list is empty
 		if (blocksInTower.isEmpty()) {
 			return 0;
 		}
-		
+
 		// Base case where there is only 1 block
 		else if (blocksInTower.size() == 1) {
 			return (blocksInTower.get(0).height);
 		}
-		
+
 		else {
-			
+
 			// The current block we are working with
 			Block curBlock = blocksInTower.get(blocksInTower.size() - 1);
-			
-			// If the current block is greater in area than the last block on the tower, add it to the tower
+
+			// If the current block is greater in area than the last block on the tower, add
+			// it to the tower
 			if (curBlock.area > blocksInTower.get(blocksInTower.size() - 2).area) {
-				
+
 				// Adds the height of this block to the height of the tower
-				return curBlock.height + maxHeightRecurK((ArrayList<Block>) blocksInTower.subList(0, blocksInTower.size() - 2));
+				return curBlock.height
+						+ maxHeightRecurK((ArrayList<Block>) blocksInTower.subList(0, blocksInTower.size() - 2));
 			}
-			
-			// If the current block is not greater in area than the last block on the tower, the height of the tower remains the same
+
+			// If the current block is not greater in area than the last block on the tower,
+			// the height of the tower remains the same
 			else {
-				
+
 				// Returns the height of the tower without this block
 				return maxHeightRecurK((ArrayList<Block>) blocksInTower.subList(0, blocksInTower.size() - 2));
 			}
 		}
 	}
-	
+
 	/**
-	 * Recursive solution
-	 * Given a block b which is on top of a stack of boxes,
-	 * finds max possible height of tower
+	 * Recursive solution Given a block b which is on top of a stack of boxes, finds
+	 * max possible height of tower
 	 * 
 	 */
 	public int maxHeightRecur(Block b) {
 		int bIndex = setOfBlocks.indexOf(b);
-		
-		//if largest block is on "top", then maxHeight is simply its height
-		if(bIndex == 0) {
+
+		// if largest block is on "top", then maxHeight is simply its height
+		if (bIndex == 0) {
 			return b.height();
 		}
 
 		int maxHeight = 0;
 		int possibleMax = 0;
-		
-		//loops through all blocks with larger base area than Block b
-		//if they can be stacked underneath b
-		for(int j = bIndex-1; j > -1; j--) {
+
+		// loops through all blocks with larger base area than Block b
+		// if they can be stacked underneath b
+		for (int j = bIndex - 1; j > -1; j--) {
 			Block blockBelow = setOfBlocks.get(j);
-			if(b.stackableOn(blockBelow)) {
+			if (b.stackableOn(blockBelow)) {
 				possibleMax = b.height() + maxHeightRecur(blockBelow);
 			}
-			
-			if(possibleMax > maxHeight) {
+
+			if (possibleMax > maxHeight) {
 				maxHeight = possibleMax;
 			}
 		}
-		
+
 		return maxHeight;
 	}
-	
+
 	/**
-	 * finds max height of all maxHeights for each block, assuming they are stored in an arrayList
+	 * finds max height of all maxHeights for each block, assuming they are stored
+	 * in an arrayList
+	 * 
 	 * @return actual max height of all the blocks
 	 */
 	public int finalMaxHeight(ArrayList<Integer> maxHeights) {
 		int max = 0;
-		for(int i = 0; i < maxHeights.size(); i++) {
-			if(maxHeights.get(i) > max) {
+		for (int i = 0; i < maxHeights.size(); i++) {
+			if (maxHeights.get(i) > max) {
 				max = maxHeights.get(i);
 			}
 		}
 		return max;
 	}
-	
+
 	/**
-	 * Class block to represent rectangular building blocks
-	 * with base dimensions of length x width and a height dimension
+	 * Dynamic programming approach to solving this problem.
+	 * 
+	 * @param b
+	 *            the block which is on top of a tower of blocks
+	 * @return the max height of the tower
+	 */
+	public int maxHeightDP(Block b) {
+
+		// Initializing DP table
+		int[] dp = new int[setOfBlocks.size()];
+
+		// Going from the top of the tower to the bottom
+		for (int i = dp.length; i > 0; i--) {
+
+			// Preparing to calculate current max
+			int max = 0;
+
+			// Current block
+			Block curBlock = setOfBlocks.get(i);
+
+			// Going through the previous blocks
+			for (int j = i + 1; i > dp.length; i--) {
+				if (curBlock.stackableOn(setOfBlocks.get(j))) {
+
+					// Checking if this is the new max for the current block
+					if (curBlock.height + dp[j] > max) {
+						max = curBlock.height + dp[j];
+					}
+				}
+			}
+
+			dp[i] = max;
+		}
+		int max = 0;
+		for (int i = 0; i < dp.length; i++) {
+			if (dp[i] > max) {
+				max = dp[i];
+			}
+		}
+
+		return max;
+	}
+
+	/**
+	 * Class block to represent rectangular building blocks with base dimensions of
+	 * length x width and a height dimension
 	 * 
 	 */
-	class Block implements Comparable<Block>{
+	class Block implements Comparable<Block> {
 		int length;
 		int width;
 		int height;
-		int area; //area of base
-		
+		int area; // area of base
+
 		/**
 		 * length should be <= width
 		 * 
@@ -219,95 +267,96 @@ public class Blocks {
 			this.length = length;
 			this.width = width;
 			this.height = height;
-			this.area = length * width; 
+			this.area = length * width;
 		}
-		
+
 		/**
 		 * @return the length of a block
 		 */
 		public int length() {
 			return length;
 		}
-		
+
 		/**
 		 * @return the width of a block
 		 */
 		public int width() {
 			return width;
 		}
-		
+
 		/**
 		 * @return the height of a block
 		 */
 		public int height() {
 			return height;
 		}
-		
+
 		/**
 		 * @return the base area of a block
 		 */
 		public int area() {
 			return area;
 		}
-		
+
 		/**
-		 * allows for built-in sorting of the blocks
-		 * in order from largest base area to smallest
+		 * allows for built-in sorting of the blocks in order from largest base area to
+		 * smallest
 		 */
 		public int compareTo(Block other) {
-			return (other.area - this.area()); //returns neg number if this block's area is bigger, pos if smaller, and 0 if equal
+			return (other.area - this.area()); // returns neg number if this block's area is bigger, pos if smaller, and
+												// 0 if equal
 		}
-		
+
 		/**
-		 * given a block, returns true if this block is stackable on top of
-		 * the other block
+		 * given a block, returns true if this block is stackable on top of the other
+		 * block
 		 */
 		public boolean stackableOn(Block other) {
 			return (width < other.width() && length < other.length());
 		}
-		
+
 		/**
-		 * returns a new block rotated such that the 1st value inputed
-		 * represents the height and
+		 * returns a new block rotated such that the 1st value inputed represents the
+		 * height and
 		 * 
-		 * the other two dimensions, the base dimensions, are placed
-		 * such that the 1st dimension <= 2nd dimension
+		 * the other two dimensions, the base dimensions, are placed such that the 1st
+		 * dimension <= 2nd dimension
 		 */
 		public Block rotation1() {
-			if(height >= width) {
+			if (height >= width) {
 				return new Block(width, height, length);
 			}
 			return new Block(height, width, length);
 		}
-		
+
 		/**
-		 * returns a new block rotated such that the 2nd value inputed
-		 * represents the height and
+		 * returns a new block rotated such that the 2nd value inputed represents the
+		 * height and
 		 * 
-		 * the other two dimensions, the base dimensions, are placed
-		 * such that the 1st dimension <= 2nd dimension
+		 * the other two dimensions, the base dimensions, are placed such that the 1st
+		 * dimension <= 2nd dimension
 		 */
 		public Block rotation2() {
-			if(height >= length) {
+			if (height >= length) {
 				return new Block(length, height, width);
 			}
 			return new Block(height, length, width);
 		}
-		
+
 		/**
-		 * returns a new block rotated such that the 3rd value inputed
-		 * represents the height and
+		 * returns a new block rotated such that the 3rd value inputed represents the
+		 * height and
 		 * 
-		 * the other two dimensions, the base dimensions, are placed
-		 * such that the 1st dimension <= 2nd dimension
+		 * the other two dimensions, the base dimensions, are placed such that the 1st
+		 * dimension <= 2nd dimension
 		 */
 		public Block rotation3() {
-			if(width >= length) {
+			if (width >= length) {
 				return new Block(length, width, height);
 			}
 			return new Block(width, length, height);
 		}
-		
+
 		/**
 		 * return a string representation of Block, which gives its three dimensions
 		 */
